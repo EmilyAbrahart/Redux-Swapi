a0165a939ca8197d8ba88c45a710e7b1f4346013
EmilyAbrahart <emilyabrahart@gmail.com>
2019-06-19 14:22:21 +0100

    remove console log and uncomment .map

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,10 +4,10 @@ import Character from "./Character";
 const CharacterList = props => {
   return (
     <ul>
-      {console.log(props.characters)}
-      {/* // props.characters.map(character => {
-      //   return <Character key={character.name} character={character} />;
-      // })} */}
+      {
+      props.characters.map(character => {
+        return <Character key={character.name} character={character} />;
+      })}
     </ul>
   );
 };
