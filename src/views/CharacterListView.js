d82d8f8c79d62e261546a843e0e9e456892e67a9
EmilyAbrahart <emilyabrahart@gmail.com>
2019-06-19 15:16:21 +0100

    initial styling

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,6 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchData } from './../actions/index';
 import { CharacterList } from '../components';
+import styled from 'styled-components';
+import {FlexFunc, color_light, header_font, text_font} from './../~reusables/reusables';
+
+const CharListWrapper = styled.div`
+${FlexFunc('column', 'center', 'center')};
+color: ${color_light};
+font-family: ${text_font};
+`
+const CharListHeader = styled.h1`
+font-size: 3rem;
+font-family: ${header_font};
+`
 
 class CharacterListView extends React.Component {
 	constructor() {
@@ -17,9 +29,10 @@ class CharacterListView extends React.Component {
 			// return something here to indicate that you are fetching data
 		}
 		return (
-			<div className="CharactersList_wrapper">
+			<CharListWrapper>
+        <CharListHeader>Star Wars Characters</CharListHeader>
 				<CharacterList characters={this.props.characters} />
-			</div>
+			</CharListWrapper>
 		);
 	}
 }
